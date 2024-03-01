bf6cd35246eac3518303ad3f9780443caf17b28b
br0fire <tgrozny@yandex.ru>
2024-03-01 19:19:55 +0000

    fix

--- projects/1a/filter_cond.py
+++ projects/1a/filter_cond.py
@@ -5,11 +5,10 @@ def filter_cond(line_dict):
     Takes a dict with field names as argument
     Returns True if conditions are satisfied
     """
-    if line_dict["if1"].isdigit():
-      if (20 < int(line_dict["if1"]) < 40):
-         return True
-      else:
-         return False
-    else:
+    if line_dict["if1"] == "":
       return False
 
+    if (20 < int(line_dict["if1"]) < 40):
+      return True
+    else:
+      return False
